1a352aa20fa306596085e4d567ec2152304a3121
Jasper Lawliet <Fair.Timothy@student.greenriver.edu>
2022-11-29 13:44:31 -0800

    Added base files for the project.

---
+++ src/main/java/com/example/SDEV2022ContinuousIntegration/Sdev2022ContinuousIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.SDEV2022ContinuousIntegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdev2022ContinuousIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdev2022ContinuousIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SDEV2022ContinuousIntegration/Sdev2022ContinuousIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SDEV2022ContinuousIntegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdev2022ContinuousIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
